3d382efca730b090cfb70e41a129075e34c8a214
Varinder Singh <48336937+varindersingh1995@users.noreply.github.com>
2023-07-17 20:41:56 -0400

    Update App.js

--- mynotes/src/App.js
+++ mynotes/src/App.js
@@ -12,7 +12,7 @@ import NotePage from './pages/NotePage';
 function App() {
   return (
     <Router>
-      <div className="container dark">
+      <div className="container">
         <div className="app">
         <Header />
         <Routes>
